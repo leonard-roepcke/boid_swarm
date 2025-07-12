dd31612c760e870bde2c75768dd0e23e915b0c66
Jakob Müller <109761119+TxJakob@users.noreply.github.com>
2025-07-12 12:47:25 +0200

    test

--- boid_handler.py
+++ boid_handler.py
@@ -4,6 +4,7 @@ class Boid_handler():
         print("Hier ist der Boid Hadler, " \
         "ich werde beim erstellen des Objekts aufgerufen.")
         self.test = "so wird eine var in einem Objekt gespeichert. Mit self. rufst du atribute in einem objekt auf. ;)"
+        print("hab mein vscode aufgesetzt, bitte funktionier einfach")
 
     def update(self):
         print("update")
